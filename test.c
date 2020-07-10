c1821ee15339eeed26d12d31e02f98549c179259
arpansahaccn <arpan.sahaccn@protonmail.com>
2020-07-09 19:34:44 -0700

    adding multiply function

--- test.c
+++ test.c
@@ -16,16 +16,23 @@
      return result ;
   }
 
+  int multi(int n1 , int n2)
+  {
+    int result ;
+     result = n1  * n2  ;
+     return result ;
+  }
 
    int main()
    {
     int num1 = 10 , num2 = 5 ;
-    int sum , divide ;
+    int sum , divide , multiply;
     sum = add(num1,num2);
     divide = div(num1,num2);
+    multiply = multi(num1,num2);
  
    printf("Sum = %d\n", sum);
-   printf("Divide = %d", divide);
+   printf("Divide = %d\n", divide);
+   printf("Multiply = %d", multiply);
    return 0;
    }
-
